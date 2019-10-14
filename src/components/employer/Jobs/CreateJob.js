ca3ed1a04422748c4133496f372024f55a807fca
Carlton Kenneth Shank III <shankles@Carltons-MacBook-Pro.local>
2019-10-14 11:43:49 -0700

    create job.js has input fields and button to add

---
+++ src/components/employer/Jobs/CreateJob.js
@@ -0,0 +1,120 @@
+import React, { Componenet } from "react";
+import axios from "axios";
+
+export default class CreateJob extends Componenet {
+	constructor() {
+		super();
+		this.state = {
+			job: [],
+			company: "",
+			city: "",
+			state: "",
+			description: "",
+			languages: ""
+		};
+	}
+	componentDidMount() {
+		axios.get("/api/getAllJobs").then((res) => {
+			console.log(res.data);
+			this.setState({ job: res.data });
+		});
+	}
+	addJob = () => {
+		axios
+			.post("/api/addNewJobs", {
+				company: this.state.company,
+				city: this.state.city,
+				state: this.state.state,
+				description: this.state.description,
+				languages: this.state.languages
+			})
+			.then((res) => {
+				this.setState({
+					job: res.data,
+					company: "",
+					city: "",
+					state: "",
+					description: "",
+					languages: ""
+				});
+			});
+	};
+	removeJob = (id) => {
+		axios
+			.delete(`/api/removeJobs${id}`)
+			.then((res) => this.setState({ job: res.data }));
+	};
+	updateJob = (id) => {
+		axios
+			.put(`/api/removeJobs/${id}`, {
+				id: this.state.id,
+				company: this.state.company,
+				city: this.state.city,
+				state: this.state.state,
+				description: this.state.description,
+				languages: this.state.languages
+			})
+			.then((res) => {
+				this.setState({
+					job: res.data,
+					company: "",
+					city: "",
+					state: "",
+					description: "",
+					languages: ""
+				});
+			});
+	};
+	render() {
+		const { company, city, state, description, languages } = this.state;
+		const displayJob = this.state.job.map((posting) => {
+			return (
+				<div>
+					<div className="listings">
+						{posting.company}
+						<br />
+						{posting.city}
+						<br />
+						{posting.state}
+						<br />
+						{posting.description}
+						<br />
+						{posting.languages}
+					</div>
+					<input
+						placeholder="company"
+						value={company}
+						onChange={(e) => this.setState({ name: e.target.value })}
+					/>
+					<input
+						placeholder="city"
+						value={city}
+						onChange={(e) => this.setState({ name: e.target.value })}
+					/>
+					<input
+						placeholder="state"
+						value={state}
+						onChange={(e) => this.setState({ name: e.target.value })}
+					/>
+					<input
+						placeholder="description"
+						value={description}
+						onChange={(e) => this.setState({ name: e.target.value })}
+					/>
+					<input
+						placeholder="languages"
+						value={languages}
+						onChange={(e) => this.setState({ name: e.target.value })}
+					/>
+					<button onClick={this.addJob}> Add a Job</button>
+				</div>
+			);
+		});
+
+		return (
+			<div>
+				<h1 className="list"> {displayJob}</h1>
+			</div>
+		);
+	}
+}
