a2e1ab8682d7e7fc305a195f7fd8561da5409836
Carlton Kenneth Shank III <shankles@Carltons-MacBook-Pro.local>
2019-10-15 14:10:07 -0700

    routes for employer and applicant with styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import JobListings from "./components/public/ActiveJobsList";
+import CreateLogin from "./components/public/auth/CreateLogin";
 import AuthApplicant from "./components/public/auth/Applicant/AuthApplicant";
 import AuthEmployer from "./components/public/auth/Employer/AuthEmployer";
-import CreateLogin from "./components/public/auth/CreateLogin";
-// import CreateJob from "./components/employer/Jobs/CreateJob";
 import "./App.scss";
-import { Route, Link, Switch, withRouter } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 
 function App() {
 	return (
@@ -20,29 +19,15 @@ function App() {
 						<li>
 							<Link to="/createAccount">Create an Account!</Link>
 						</li>
-						{/* <li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/applicantRegister">Apply for Jobs</Link>
-						</li>
-						<li>
-							<Link to="/employerRegister">Post Jobs</Link>
-						</li>
-						<li>
-							<Link to="/appProfile">Applicant Profile</Link>
-						</li>
-						<li>
-							<Link to="/employerProfile">Employer Profile</Link>
-						</li> */}
 					</ul>
 				</nav>
 			</header>
 
 			<Switch>
 				<Route exact path="/" component={JobListings} />
-				<Route path="/createAccount" component={CreateLogin} />
+				<Route path="/createAccount" exact component={CreateLogin} />
 				<Route path="/EmployerLogin" component={AuthEmployer} />
+				<Route path="/ApplicantLogin" component={AuthApplicant} />
 			</Switch>
 		</div>
 	);
