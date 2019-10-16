c6fb52ddd6eebf65b7d23129b14e60fd6027d2f2
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2019-10-16 16:10:13 -0700

    search bar done

--- src/components/public/ActiveJobsList.js
+++ src/components/public/ActiveJobsList.js
@@ -3,94 +3,109 @@ import axios from "axios";
 import "./activejobs.scss";
 
 export default class JobListings extends Component {
-	constructor() {
-		super();
-		this.state = {
-			job: [],
-			company: "",
-			city: "",
-			state: "",
-			description: "",
-			languages: ""
-		};
-	}
-	componentDidMount() {
-		axios.get("/api/getAllJobs").then((res) => {
-			this.setState({ job: res.data });
-		});
-	}
-	addJob = () => {
-		axios
-			.post("/api/addNewJobs", {
-				company: this.state.company,
-				city: this.state.city,
-				state: this.state.state,
-				description: this.state.description,
-				languages: this.state.languages
-			})
-			.then((res) => {
-				this.setState({
-					job: res.data,
-					company: "",
-					city: "",
-					state: "",
-					description: "",
-					languages: ""
-				});
-			});
-	};
-	removeJob = (id) => {
-		axios
-			.delete(`/api/removeJobs${id}`)
-			.then((res) => this.setState({ job: res.data }));
-	};
-	updateJob = (id) => {
-		axios
-			.put(`/api/removeJobs/${id}`, {
-				id: this.state.id,
-				company: this.state.company,
-				city: this.state.city,
-				state: this.state.state,
-				description: this.state.description,
-				languages: this.state.languages
-			})
-			.then((res) => {
-				this.setState({
-					job: res.data,
-					company: "",
-					city: "",
-					state: "",
-					description: "",
-					languages: ""
-				});
-			});
-	};
+  constructor() {
+    super();
+    this.state = {
+      job: [],
+      company: "",
+      city: "",
+      state: "",
+      description: "",
+      languages: "",
+      search: ""
+    };
+  }
+  componentDidMount() {
+    axios.get("/api/getAllJobs").then(res => {
+      this.setState({ job: res.data });
+    });
+  }
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) });
+  }
+  addJob = () => {
+    axios
+      .post("/api/addNewJobs", {
+        company: this.state.company,
+        city: this.state.city,
+        state: this.state.state,
+        description: this.state.description,
+        languages: this.state.languages
+      })
+      .then(res => {
+        this.setState({
+          job: res.data,
+          company: "",
+          city: "",
+          state: "",
+          description: "",
+          languages: ""
+        });
+      });
+  };
+  removeJob = id => {
+    axios
+      .delete(`/api/removeJobs${id}`)
+      .then(res => this.setState({ job: res.data }));
+  };
+  updateJob = id => {
+    axios
+      .put(`/api/removeJobs/${id}`, {
+        id: this.state.id,
+        company: this.state.company,
+        city: this.state.city,
+        state: this.state.state,
+        description: this.state.description,
+        languages: this.state.languages
+      })
+      .then(res => {
+        this.setState({
+          job: res.data,
+          company: "",
+          city: "",
+          state: "",
+          description: "",
+          languages: ""
+        });
+      });
+  };
 
-	render() {
-		const displayJob = this.state.job.map((posting, i) => {
-			return (
-				<div key={i}>
-					<div className="listings">
-						{posting.company}
-						<br />
-						{posting.city}
-						<br />
-						{posting.state}
-						<br />
-						{posting.description}
-						<br />
-						{posting.languages}
-						<br />
-						<button>apply</button>
-					</div>
-				</div>
-			);
-		});
+  render() {
+    const displayJob = this.state.job
+      .filter(searching => {
+        return (
+          searching.languages.toLowerCase().indexOf(this.state.search) !== -1
+        );
+      })
+      .map((posting, i) => {
+        return (
+          <div key={i}>
+            <div className="listings">
+              {posting.company}
+              <br />
+              {posting.city}
+              <br />
+              {posting.state}
+              <br />
+              {posting.description}
+              <br />
+              {posting.languages}
+              <br />
+              <button>apply</button>
+            </div>
+          </div>
+        );
+      });
 
-		return (
-			<div>
-				<h1 className="list"> {displayJob}</h1>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)}
+        />
+        <h1 className="list"> {displayJob}</h1>
+      </div>
+    );
+  }
 }
