3ece1c0000e9b7658f55e21c4202f5dd14e6cf15
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2019-10-17 15:30:05 -0700

    worked on nav bar links

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React from "react";
 import JobListings from "./components/public/ActiveJobsList";
-import CreateLogin from "./components/public/auth/CreateLogin";
 import AuthApplicant from "./components/public/auth/Applicant/AuthApplicant";
 import AuthEmployer from "./components/public/auth/Employer/AuthEmployer";
+import Navbar from "./components/Navbar/Navbar";
 import "./App.scss";
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <div className="logo">LOGO</div>
-        <nav className="links">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/createAccount">Create an Account!</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
       </header>
 
       <Switch>
         <Route exact path="/" component={JobListings} />
-        <Route path="/createAccount" exact component={CreateLogin} />
         <Route path="/EmployerLogin" component={AuthEmployer} />
         <Route path="/ApplicantLogin" component={AuthApplicant} />
       </Switch>
