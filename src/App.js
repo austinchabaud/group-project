f7332df851e07995d9543bd0f681a76281306d62
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2019-10-18 14:35:06 -0700

    trying to fix navbar

--- src/App.js
+++ src/App.js
@@ -4,34 +4,19 @@ import AuthApplicant from "./components/public/auth/Applicant/AuthApplicant";
 import AuthEmployer from "./components/public/auth/Employer/AuthEmployer";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.scss";
-import { Route, Link, Switch } from "react-router-dom";
-import MyAppliedJobs from "./components/applicant/profile/ProfileMyAppliedJobs";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <div className="logo">LOGO</div>
-        <nav className="links">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/createAccount">Create an Account</Link>
-            </li>
-            <li>
-              <Link to="/ProfileMyAppliedJobs">Applied Jobs</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
       </header>
 
       <Switch>
         <Route exact path="/" component={JobListings} />
         <Route path="/EmployerLogin" component={AuthEmployer} />
         <Route path="/ApplicantLogin" component={AuthApplicant} />
-        <Route path="/ProfileMyAppliedJobs" component={MyAppliedJobs} />
       </Switch>
     </div>
   );
