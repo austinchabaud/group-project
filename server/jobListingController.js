67681cee87c799c862486996ef0c0f4018f46c3d
Carlton Kenneth Shank III <shankles@Carltons-MacBook-Pro.local>
2019-10-22 11:44:28 -0700

    timestamp

--- server/jobListingController.js
+++ server/jobListingController.js
@@ -1,56 +1,82 @@
 module.exports = {
-  getAllJobs: (req, res) => {
-    const db = req.app.get("db");
-    db.Employer.joblisting.allJobs().then(jobs => res.status(200).send(jobs));
-  },
-  addJob: (req, res) => {
-    const db = req.app.get("db");
-    const { title, company, city, state, description, languages } = req.body;
-    db.Employer.joblisting
-      .addJob([title, company, city, state, description, languages])
-      .then(jobs => res.status(200).send(jobs));
-  },
-  updateJob: (req, res) => {
-    const db = req.app.get("db");
-    const { id } = req.params;
-    const { title, company, city, state, description, languages } = req.body;
-    db.Employer.joblisting
-      .updateJobs([id, title, company, city, state, description, languages])
-      .then(jobs => res.status(200).send(jobs));
-  },
-  removeJob: (req, res) => {
-    const db = req.app.get("db");
-    const { id } = req.params;
-    db.Employer.joblisting
-      .deleteJob(id)
-      .then(jobs => res.status(200).send(jobs));
-  },
-  getJobsByEmployer: (req, res) => {
-    const db = req.app.get("db");
-    const { name } = req.session;
-    db.Employer.joblisting
-      .get_jobs_by_employer(name)
-      .then(jobs => res.status(200).send(jobs));
-  },
-  getJobsByApplicant: (req, res) => {
-    const db = req.app.get("db");
-    const { id } = req.session.user;
-    console.log(55555, req.session);
-    db.Applicant.get_jobs_by_applicant(id)
-      .then(jobs => res.status(200).send(jobs))
-      .catch(err => {
-        console.log(err);
-      });
-  },
-  apply: (req, res) => {
-    const db = req.app.get("db");
-    // const { id } = req.session.user;
-    const { id } = req.session.user;
-    const { job_id } = req.body;
-    console.log(66666666, req.session.user.id);
-    console.log(job_id, id);
-    db.Applicant.applicant_apply([job_id, id]).then(jobs =>
-      res.status(200).send(jobs)
-    );
-  }
+	getAllJobs: (req, res) => {
+		const db = req.app.get("db");
+		db.Employer.joblisting.allJobs().then((jobs) => res.status(200).send(jobs));
+	},
+	addJob: (req, res) => {
+		const db = req.app.get("db");
+		console.log(req.body.date);
+		const {
+			title,
+			company,
+			city,
+			state,
+			description,
+			languages,
+			date_added
+		} = req.body;
+		db.Employer.joblisting
+			.addJob([title, company, city, state, description, languages, date_added])
+			.then((jobs) => res.status(200).send(jobs));
+	},
+	updateJob: (req, res) => {
+		const db = req.app.get("db");
+		const { id } = req.params;
+		const {
+			title,
+			company,
+			city,
+			state,
+			description,
+			languages,
+			date_added
+		} = req.body;
+		db.Employer.joblisting
+			.updateJobs([
+				id,
+				title,
+				company,
+				city,
+				state,
+				description,
+				languages,
+				date_added
+			])
+			.then((jobs) => res.status(200).send(jobs));
+	},
+	removeJob: (req, res) => {
+		const db = req.app.get("db");
+		const { id } = req.params;
+		db.Employer.joblisting
+			.deleteJob(id)
+			.then((jobs) => res.status(200).send(jobs));
+	},
+	getJobsByEmployer: (req, res) => {
+		const db = req.app.get("db");
+		const { name } = req.session;
+		db.Employer.joblisting
+			.get_jobs_by_employer(name)
+			.then((jobs) => res.status(200).send(jobs));
+	},
+	getJobsByApplicant: (req, res) => {
+		const db = req.app.get("db");
+		const { id } = req.session.user;
+		console.log(55555, req.session);
+		db.Applicant.get_jobs_by_applicant(id)
+			.then((jobs) => res.status(200).send(jobs))
+			.catch((err) => {
+				console.log(err);
+			});
+	},
+	apply: (req, res) => {
+		const db = req.app.get("db");
+		// const { id } = req.session.user;
+		const { id } = req.session.user;
+		const { job_id } = req.body;
+		console.log(66666666, req.session.user.id);
+		console.log(job_id, id);
+		db.Applicant.applicant_apply([job_id, id]).then((jobs) =>
+			res.status(200).send(jobs)
+		);
+	}
 };
