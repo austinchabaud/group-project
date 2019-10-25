c6d173deffaa75885f6fd1f3a61603044ef145e9
Gmack <mack.gerrit@gmail.com>
2019-10-25 09:37:06 -0700

    job added rendering

--- db/Employer/joblisting/addJob.sql
+++ db/Employer/joblisting/addJob.sql
@@ -1,2 +1,8 @@
-insert into jobs(title, company, city, state, description, languages, date_added)
-values ($1, $2, $3, $4, $5, $6, $7);
+insert into jobs
+    (title, company, city, state, description, languages, date_added)
+values
+    ($1, $2, $3, $4, $5, $6, $7);
+
+select *
+from jobs
+where company = $2;
